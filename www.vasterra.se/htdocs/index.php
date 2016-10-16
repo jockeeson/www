14cd0cdae89f87ff7cd3c02bb0939aa5f970d2d6
jockeeson <jockeeriksson@me.com>
2016-10-16 12:40:32 +0200

    Switched out index.php and some new css style sheets

--- www.vasterra.se/htdocs/index.php
+++ www.vasterra.se/htdocs/index.php
@@ -1,69 +1,141 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"> 
-<html> 
- <head>
-  <title>Hem</title>
-  <meta charset="UTF-8" />
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <meta name="apple-mobile-web-app-capable" content="yes"/> 
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
- </head> 
- <body>
-  <?php 
-   if (isset($_POST['1ON'])) { 
-      exec("/home/jocke/python/433MHz/TransmitRF_nexa.py a_on");
-   }
-   if (isset($_POST['2ON'])) { 
-      exec("/home/jocke/python/433MHz/TransmitRF_nexa.py b_on");
-   }
-   if (isset($_POST['3ON'])) { 
-      exec("/home/jocke/python/433MHz/TransmitRF_nexa.py c_on");
-   }
-   if (isset($_POST['1OFF'])) { 
-      exec("/home/jocke/python/433MHz/TransmitRF_nexa.py a_off");
-   }
-   if (isset($_POST['2OFF'])) { 
-      exec("/home/jocke/python/433MHz/TransmitRF_nexa.py b_off");
-   }
-   if (isset($_POST['3OFF'])) { 
-      exec("/home/jocke/python/433MHz/TransmitRF_nexa.py c_off");
-   }
-   if (isset($_POST['allOFF'])) { 
-      exec("/home/jocke/python/433MHz/TransmitRF_nexa.py all_off");
-   }
-  ?>
- 
-  <div class="header">
-   <h1>Hem</h1>
+<!DOCTYPE html>
+<html>
+<title>Hem</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="w3.css">
+<link rel="stylesheet" href="w3-theme-black.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+<style>
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+.w3-sidenav a,.w3-sidenav h4 {padding: 12px;}
+.w3-navbar li a {
+    padding-top: 12px;
+    padding-bottom: 12px;
+}
+</style>
+<body>
+
+<!-- Navbar -->
+<div class="w3-top">
+  <ul class="w3-navbar w3-theme w3-top w3-left-align w3-large">
+    <li class="w3-opennav w3-right w3-hide-large">
+      <a class="w3-hover-white w3-large w3-theme-l1" href="javascript:void(0)" onclick="w3_open()"><i class="fa fa-bars"></i></a>
+    </li>
+    <li><a href="#" class="w3-theme-l1">Logo</a></li>
+    <li class="w3-hide-small"><a href="#" class="w3-hover-white">About</a></li>
+    <li class="w3-hide-small"><a href="#" class="w3-hover-white">Values</a></li>
+    <li class="w3-hide-small"><a href="#" class="w3-hover-white">News</a></li>
+    <li class="w3-hide-small"><a href="#" class="w3-hover-white">Contact</a></li>
+    <li class="w3-hide-medium w3-hide-small"><a href="#" class="w3-hover-white">Clients</a></li>
+    <li class="w3-hide-medium w3-hide-small"><a href="#" class="w3-hover-white">Partners</a></li>
+  </ul>
+</div>
+
+<!-- Sidenav -->
+<nav class="w3-sidenav w3-collapse w3-theme-l5 w3-animate-left" style="z-index:3;width:250px;margin-top:51px;" id="mySidenav">
+  <a href="javascript:void(0)" onclick="w3_close()" class="w3-right w3-xlarge w3-padding-large w3-hover-black w3-hide-large" title="close menu">
+    <i class="fa fa-remove"></i>
+  </a>
+  <h4><b>Menu</b></h4>
+  <a href="#" class="w3-hover-black">Link</a>
+  <a href="#" class="w3-hover-black">Link</a>
+  <a href="#" class="w3-hover-black">Link</a>
+  <a href="#" class="w3-hover-black">Link</a>
+</nav>
+
+<!-- Overlay effect when opening sidenav on small screens -->
+<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+<!-- Main content: shift it to the right by 250 pixels when the sidenav is visible -->
+<div class="w3-main" style="margin-left:250px">
+
+  <div class="w3-row w3-padding-64">
+    <div class="w3-twothird w3-container">
+      <h1 class="w3-text-teal">Heading</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    <div class="w3-third w3-container">
+      <p class="w3-border w3-padding-large w3-padding-32 w3-center">AD</p>
+      <p class="w3-border w3-padding-large w3-padding-64 w3-center">AD</p>
+    </div>
   </div>
 
-  <div class="row">
+  <div class="w3-row">
+    <div class="w3-twothird w3-container">
+      <h1 class="w3-text-teal">Heading</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    <div class="w3-third w3-container">
+      <p class="w3-border w3-padding-large w3-padding-32 w3-center">AD</p>
+      <p class="w3-border w3-padding-large w3-padding-64 w3-center">AD</p>
+    </div>
+  </div>
 
-   <div class="col-l-5 menu">
-    <ul>
-     <li>Lampor</li>
+  <div class="w3-row w3-padding-64">
+    <div class="w3-twothird w3-container">
+      <h1 class="w3-text-teal">Heading</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    <div class="w3-third w3-container">
+      <p class="w3-border w3-padding-large w3-padding-32 w3-center">AD</p>
+      <p class="w3-border w3-padding-large w3-padding-64 w3-center">AD</p>
+    </div>
+  </div>
+
+  <!-- Pagination -->
+  <div class="w3-center w3-padding-64">
+    <ul class="w3-pagination">
+      <li><a class="w3-black" href="#">1</a></li>
+      <li><a class="w3-hover-black" href="#">2</a></li>
+      <li><a class="w3-hover-black" href="#">3</a></li>
+      <li><a class="w3-hover-black w3-hide-small" href="#">4</a></li>
+      <li><a class="w3-hover-black w3-hide-small" href="#">5</a></li>
+      <li><a class="w3-hover-black" href="#">»</a></li>
     </ul>
-   </div>
-
-	<div class="col-l-7 main">
-	 <form method="post">
-	  <p>
-		<button class="button_on" name="1ON">1 ON</button>&nbsp;
- 		<button class="button_off" name="1OFF">1 OFF</button><br><br>
-      <button class="button_on" name="2ON">2 ON</button>&nbsp;
-      <button class="button_off" name="2OFF">2 OFF</button><br><br>
-      <button class="button_on" name="3ON">3 ON</button>&nbsp;
-      <button class="button_off" name="3OFF">3 OFF</button><br><br>
-      <button class="button_off" name="allOFF">All OFF</button>&nbsp;
-     </p>
-     <p>
- 		<a href="/secret">Secret</a>
-	  </p>
-    </form>
-   </div>
-  
   </div>
 
- </body>
-</html>
+  <footer id="myFooter">
+    <div class="w3-container w3-theme-l2 w3-padding-32">
+      <h4>Footer</h4>
+    </div>
 
+    <div class="w3-container w3-theme-l1">
+      <p>Powered by <a href="http://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+    </div>
+  </footer>
+
+<!-- END MAIN -->
+</div>
+
+<script>
+// Get the Sidenav
+var mySidenav = document.getElementById("mySidenav");
+
+// Get the DIV with overlay effect
+var overlayBg = document.getElementById("myOverlay");
+
+// Toggle between showing and hiding the sidenav, and add overlay effect
+function w3_open() {
+    if (mySidenav.style.display === 'block') {
+        mySidenav.style.display = 'none';
+        overlayBg.style.display = "none";
+    } else {
+        mySidenav.style.display = 'block';
+        overlayBg.style.display = "block";
+    }
+}
+
+// Close the sidenav with the close button
+function w3_close() {
+    mySidenav.style.display = "none";
+    overlayBg.style.display = "none";
+}
+</script>
+
+</body>
+</html>
 
