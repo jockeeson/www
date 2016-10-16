71b4ef3b83f9494a9c115f79be5f28d1eb56a0ac
jockeeson <jockeeriksson@me.com>
2016-10-16 09:46:17 +0200

    Added secret folder

---
+++ www.vasterra.se/htdocs/secret/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+ <title>Hemligt</title>
+ <meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+ <link rel="stylesheet" type="text/css" href="/style.css" />
+</head>
+<body>
+
+Hejsan
+</br>
+En hemlig sida.
+
+</body>
+</html>
